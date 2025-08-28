c663ff61672f583f5e8809ec78092fdd71de1324
Andre Giske <AndreGiske@Mac.lan>
2025-08-27 18:21:49 -0700

    resolving warnings

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,6 @@
 # app/schemas.py
 
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, ConfigDict
 
 class UserCreate(BaseModel):
     email: EmailStr
@@ -9,6 +9,7 @@ class UserCreate(BaseModel):
 class UserOut(BaseModel):
     id: int
     email: EmailStr
+    model_config = ConfigDict(from_attributes=True)
 
 class LoginRequest(BaseModel):
 	email: EmailStr
