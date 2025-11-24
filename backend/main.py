186a79c7168b755f48b60f0e185dffb6bc436eb2
Andre Giske <agiske@ucsd.edu>
2025-11-24 12:33:18 -0800

    working user apis

--- backend/main.py
+++ backend/main.py
@@ -5,6 +5,8 @@ Create FastAPI app, mount routers and middlewares
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
+from contextlib import asynccontextmanager
+
 from backend.config import settings
 
 from backend import models
@@ -16,7 +18,19 @@ from backend.api.routes import users
 # from backend.api.routes import feed as feed_routes
 # from backend.api.routes import ingest as ingest_routes
 
-app = FastAPI()
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+	'''
+	Startup / shutdown events
+	'''
+	# Startup actions
+	Base.metadata.create_all(bind=engine)
+
+	yield
+	# Shutdown actions
+	# (none)
+
+app = FastAPI(lifespan=lifespan)
 
 origins = [o.strip() for o in settings.CORS_ORIGINS.split(",") if o.strip()]
 app.add_middleware(
@@ -32,11 +46,3 @@ app.include_router(users.router, tags=["users"])
 # app.include_router(interactions_routes.router, tags=["interactions"])
 # app.include_router(feed_routes.router, tags=["feed"])
 # app.include_router(ingest_routes.router, tags=["ingest"])
-
-@app.on_event("startup")
-def on_startup():
-	'''
-	Actions to perform on startup
-	- create database tables
-	'''
-	Base.metadata.create_all(bind=engine)
