2c14621d0bf148e6f04c2a60af57ccf1dca09559
Andre Giske <agiske@ucsd.edu>
2025-11-30 15:49:34 -0800

    normalizing data function

--- backend/services/ingest.py
+++ backend/services/ingest.py
@@ -1,10 +1,12 @@
 '''
 News ingestion services.
 '''
+from datetime import datetime
 
 def fetch_newsapi_articles(api_key, query, page_size=3):
 	'''
-	Fetch articles from News API based on a query.'''
+	Fetch articles from News API based on a query
+	'''
 	import requests
 
 	url = "https://newsapi.org/v2/everything"
@@ -22,4 +24,22 @@ def normalize_article_data(raw):
 	Normalize raw article data from ingestion sources into
 	our article database schema
 	'''
-	
+	published_at = raw.get("publishedAt")
+	if published_at:
+		try:
+			published_at = datetime.fromisoformat(published_at.replace("Z", "+00:00"))
+		except ValueError:
+			published_at = None
+	else:
+		published_at = None
+
+	return {
+		"title": raw.get("title") or "Untitled",
+		"content": raw.get("content"),
+		"source": raw.get("source", {}).get("name"),
+		"url": raw.get("url"),
+		"published_at": published_at,
+
+		# Grab keywords via extraction later
+		"keywords": None 
+	}
