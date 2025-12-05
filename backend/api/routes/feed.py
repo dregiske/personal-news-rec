72001d3e26dccf234fc681c335d255ff316fd80a
Andre Giske <agiske@ucsd.edu>
2025-12-05 02:12:20 -0800

    wired up recommender to backend

--- backend/api/routes/feed.py
+++ backend/api/routes/feed.py
@@ -7,7 +7,7 @@ from sqlalchemy.orm import Session
 from typing import List
 
 from backend.database import get_database
-from backend.models import User as UserModel
+from backend.models import Article, User as UserModel
 from backend.services.auth import get_current_user
 from backend.services.recommendation import hybrid_recommend_articles
 
@@ -34,4 +34,20 @@ def get_personalized_feed(
 		db=db,
 		k=limit
 	)
-	return articles
+	if not articles:
+		return []
+	
+	id_to_score = {article_id: score for article_id, score in articles}
+	article_ids = list(id_to_score.keys())
+	articles = (
+		db.query(Article)
+			.filter(Article.id.in_(article_ids))
+			.all()
+	)
+	article_by_id = {article.id: article for article in articles}
+	ordered_articles = [
+		article_by_id[article_id]
+		for article_id, _ in article_ids
+		if article_id in article_by_id
+	]
+	return ordered_articles
