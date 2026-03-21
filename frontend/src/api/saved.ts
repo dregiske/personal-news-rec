90b5105f515cfaefdecfa36c788f1fdafff1ecc0
Andre Giske <agiske@ucsd.edu>
2026-03-21 04:50:53 -0700

    api saves

---
+++ frontend/src/api/saved.ts
@@ -0,0 +1,16 @@
+import api, { API_BASE } from './api';
+import type { SavedArticle } from '../types';
+
+export async function fetchSaved(): Promise<SavedArticle[]> {
+  const res = await api.get<SavedArticle[]>(`${API_BASE}/me/saved`);
+  return res.data;
+}
+
+export async function saveArticle(articleId: number): Promise<SavedArticle> {
+  const res = await api.post<SavedArticle>(`${API_BASE}/me/saved/${articleId}`);
+  return res.data;
+}
+
+export async function unsaveArticle(articleId: number): Promise<void> {
+  await api.delete(`${API_BASE}/me/saved/${articleId}`);
+}
