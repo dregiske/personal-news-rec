3bf11989062832b5bf5fa52b0ae6a9c91f1b3591
Andre Giske <agiske@ucsd.edu>
2026-03-27 03:59:42 -0700

    keywords extracts from constants

--- backend/services/keywords.py
+++ backend/services/keywords.py
@@ -1,29 +1,10 @@
 import re
 from collections import Counter
 from backend.config import settings
+from backend.constants import STOP_WORDS
 
-STOP_WORDS = {
-	"a", "about", "above", "after", "again", "against", "all", "am", "an", "and",
-	"any", "are", "aren't", "as", "at", "be", "because", "been", "before", "being",
-	"below", "between", "both", "but", "by", "can't", "cannot", "could", "couldn't",
-	"did", "didn't", "does", "doesn't", "doing", "don't", "down", "during", "each",
-	"few", "for", "from", "further", "had", "hadn't", "has", "hasn't", "have", "haven't",
-	"having", "he", "he'd", "he'll", "he's", "her", "here", "here's", "hers", "herself",
-	"him", "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm", "i've", "if",
-	"in", "into", "is", "isn't", "it", "it's", "its", "itself", "just", "ll", "ma", "me",
-	"mightn't", "more", "most", "mustn't", "my", "myself", "needn't", "no", "nor", "not",
-	"now", "of", "off", "on", "once", "only", "or", "other", "our", "ours", "ourselves", "out",
-	"over", "own", "re", "s", "same", "shan't", "she", "she'd", "she'll", "she's", "should",
-	"shouldn't", "so", "some", "such", "t", "than", "that", "that's", "the", "their", "theirs",
-	"them", "themselves", "then", "there", "there's", "these", "they", "they'd", "they'll", "they're", "they've",
-	"this", "those", "through", "to", "too", "under", "until", "up", "ve", "very", "was", "wasn't",
-}
 
 def extract_keywords(text: str, max_keywords: int) -> list[str]:
-	'''
-	Extracts keywords from the given text by removing 
-	stop words and selecting the most common words.
-	'''
 	if not text:
 		return []
 	
